b86885bf813e39907c5d7a1f24a1bb48c696394c
kunimoto <akira.kunimoto@gmail.com>
2024-06-10 23:40:34 +0900

    学年を追加。birthyearをdatetime型にしたら表示に時刻が出るようになった。

--- analize_player.py
+++ analize_player.py
@@ -5,7 +5,7 @@ def load_data():
     df = pd.read_csv('player_info.csv')
     return df
 
-def filter_data(df, name, team, high_school, birthplace, birthday, draft_year):
+def filter_data(df, name, team, high_school, birthplace, birthday, schoolyear ,draft_year):
     if name:
         df = df[df['選手名'].str.contains(name)]
     if team != '全て':
@@ -16,15 +16,17 @@ def filter_data(df, name, team, high_school, birthplace, birthday, draft_year):
         df = df[df['出身地'].str.contains(birthplace)]
     if birthday:
         df = df[df['birthday'].str.contains(birthday)]
+    if schoolyear:
+        df = df[df['学年'].str.contains(schoolyear)]
     if draft_year:
         df = df[df['ドラフト年'].str.contains(draft_year)]
     return df
 
 def display_filtered_data(df):
-    st.write(df[['選手名', '所属チーム', 'birthday', '出身高校']])
+    st.write(df[['選手名', '所属チーム', 'birthday2', '出身高校']])
 
 def display_filtered_data2(df):
-    st.write(df[['選手名', '背番号', '所属チーム', '出身高校','出身地','生年月日','ドラフト年','ドラフト順位', '身長','血液型','経歴' ]])
+    st.write(df[['選手名', '背番号', '所属チーム', '出身高校','出身地','birthday2','学年','ドラフト年','ドラフト順位', '身長','血液型','経歴' ]])
 
 
 def display_aggregated_data(df, select_item):
@@ -41,6 +43,12 @@ def main():
     st.title('NPB選手情報')
     df = load_data()
     df = df.drop_duplicates(subset='選手番号')
+    # 誕生日をdatetime型に変換
+    df['birthday2'] = pd.to_datetime(df['birthday'])
+    # 学年度を計算
+    df['学年度'] = df['birthday2'].apply(lambda x: x.year-1 if x.month < 4 else x.year)
+    df['学年'] = df['学年度'].astype(str) + '年度'      
+    
     # =======
     st.write('### フィルタ表示')
     name = st.text_input('選手名')    
@@ -50,30 +58,21 @@ def main():
     high_school = st.text_input('出身高校')
     birthplace = st.text_input('出身地')
     birthday = st.text_input('生年月日')
+    schoolyear = st.text_input('学年')
     draft_year = st.text_input('ドラフト年')
     
-    df = filter_data(df, name, team, high_school, birthplace, birthday, draft_year)
+    df = filter_data(df, name, team, high_school, birthplace, birthday, schoolyear,draft_year)
     
     display_filtered_data2(df)
 
     st.write('### 集計')    
-    select_item = st.selectbox('集計項目選択', ['所属チーム', '出身高校', '出身地', 'ドラフト年', 'ドラフト順位', '身長', '血液型','選手名',''])
+    select_item = st.selectbox('集計項目選択', ['所属チーム', '出身高校', '出身地','学年', 'ドラフト年', 'ドラフト順位', '身長', '血液型','選手名',''])
     display_aggregated_data(df, select_item)
 
 if __name__ == '__main__':
     main()
     
     
-# このコードを実行すると、ブラウザが立ち上がり、所属チーム、年齢、出身高校の条件でフィルターされた選手情報が表示される。
-# また、選手情報はplayer_info.csvから読み込まれる。
-# このコードを実行するためには、streamlitをインストールする必要がある。以下のコマンドでインストールできる。
-# pip install streamlit
-# その後、このコードを保存して、以下のコマンドで実行する。
-# streamlit run analize_player.py
-# すると、ブラウザが立ち上がり、選手情報が表示される。
-# このコードを実行するためには、player_info.csvが必要である。
-# このcsvファイルは、player_info.csvという名前で保存しておく。
-
 
 
 
